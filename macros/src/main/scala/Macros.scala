fa9d4f1a2b0f27b03ca7f824e0d19be0928c69fb
Andrzej Giniewicz <Andrzej.Giniewicz@pwr.wroc.pl>
2014-07-20 13:18:38 +0200

    Switch to macro bundles, still works

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -1,11 +1,12 @@
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
-object Macros {
-  def impl(c: Context) = {
-    import c.universe._
-    c.Expr[Unit](q"""println("Hello World")""")
-  }
+class Impl(val c: Context) {
+  import c.universe._
 
-  def hello: Unit = macro impl
+  def hello = c.Expr[Unit](q"""println("Hello World")""")
+}
+
+object Macros {
+  def hello: Unit = macro Impl.hello
 }
