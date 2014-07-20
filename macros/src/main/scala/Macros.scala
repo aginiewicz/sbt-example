1d8e042100ec38093fa0fa3c16928222631ff21d
Andrzej Giniewicz <Andrzej.Giniewicz@pwr.wroc.pl>
2014-07-20 13:20:32 +0200

    Add another macro, don't call it, yet still Scoverage fails

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -4,9 +4,13 @@ import scala.reflect.macros.Context
 class Impl(val c: Context) {
   import c.universe._
 
+  def poly[T: c.WeakTypeTag] = c.literal(c.weakTypeOf[T].toString)
+
   def hello = c.Expr[Unit](q"""println("Hello World")""")
 }
 
 object Macros {
+  def poly[T] = macro Impl.poly[T]
+
   def hello: Unit = macro Impl.hello
 }
